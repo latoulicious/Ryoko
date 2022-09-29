b67e27a740d37f7dec6ff1a774aa3e76acfb27e7
latoulicious <count.kramm48@gmail.com>
2022-09-30 00:18:24 +0700

    ⚡️

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,9 +19,9 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
+        <title>Ryoko</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/rocket.svg" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import Header from '../components/Header.tsx';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <Header />
     </>
   );
 }
